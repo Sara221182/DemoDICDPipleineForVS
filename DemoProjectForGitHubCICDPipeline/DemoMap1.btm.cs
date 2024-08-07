18186ea6173fe5d34622b1d9b158669b0118bb80
Shashank Bhosekar <sbhosekar@corcentric.com>
2024-08-07 18:09:42 -0400

    Added multiply for test

--- DemoProjectForGitHubCICDPipeline/DemoMap1.btm.cs
+++ DemoProjectForGitHubCICDPipeline/DemoMap1.btm.cs
@@ -6,7 +6,7 @@ namespace DemoProjectForGitHubCICDPipeline {
     public sealed class DemoMap1 : global::Microsoft.XLANGs.BaseTypes.TransformBase {
         
         private const string _strMap = @"<?xml version=""1.0"" encoding=""UTF-16""?>
-<xsl:stylesheet xmlns:xsl=""http://www.w3.org/1999/XSL/Transform"" xmlns:msxsl=""urn:schemas-microsoft-com:xslt"" xmlns:var=""http://schemas.microsoft.com/BizTalk/2003/var"" exclude-result-prefixes=""msxsl var s0"" version=""1.0"" xmlns:s0=""http://DemoProjectForGitHubCICDPipeline.Input1"" xmlns:ns0=""http://DemoProjectForGitHubCICDPipeline.Output1"">
+<xsl:stylesheet xmlns:xsl=""http://www.w3.org/1999/XSL/Transform"" xmlns:msxsl=""urn:schemas-microsoft-com:xslt"" xmlns:var=""http://schemas.microsoft.com/BizTalk/2003/var"" exclude-result-prefixes=""msxsl var s0 userCSharp"" version=""1.0"" xmlns:s0=""http://DemoProjectForGitHubCICDPipeline.Input1"" xmlns:ns0=""http://DemoProjectForGitHubCICDPipeline.Output1"" xmlns:userCSharp=""http://schemas.microsoft.com/BizTalk/2003/userCSharp"">
   <xsl:output omit-xml-declaration=""yes"" method=""xml"" version=""1.0"" />
   <xsl:template match=""/"">
     <xsl:apply-templates select=""/s0:Root"" />
@@ -14,14 +14,68 @@ namespace DemoProjectForGitHubCICDPipeline {
   <xsl:template match=""/s0:Root"">
     <ns0:Root>
       <xsl:for-each select=""Record"">
+        <xsl:variable name=""var:v1"" select=""userCSharp:MathMultiply(string(Test1/text()) , &quot;10&quot;)"" />
         <Record>
           <OutTest2>
-            <xsl:value-of select=""Test1/text()"" />
+            <xsl:value-of select=""$var:v1"" />
           </OutTest2>
         </Record>
       </xsl:for-each>
     </ns0:Root>
   </xsl:template>
+  <msxsl:script language=""C#"" implements-prefix=""userCSharp""><![CDATA[
+public string MathMultiply(string param0, string param1)
+{
+	System.Collections.ArrayList listValues = new System.Collections.ArrayList();
+	listValues.Add(param0);
+	listValues.Add(param1);
+	double ret = 1;
+	bool first = true;
+	foreach (string obj in listValues)
+	{
+		double d = 0;
+		if (IsNumeric(obj, ref d))
+		{
+			if (first)
+			{
+				first = false;
+				ret = d;
+			}
+			else
+			{
+				ret *= d;
+			}
+		}
+		else
+		{
+			return """";
+		}
+	}
+	return ret.ToString(System.Globalization.CultureInfo.InvariantCulture);
+}
+
+
+public bool IsNumeric(string val)
+{
+	if (val == null)
+	{
+		return false;
+	}
+	double d = 0;
+	return Double.TryParse(val, System.Globalization.NumberStyles.AllowThousands | System.Globalization.NumberStyles.Float, System.Globalization.CultureInfo.InvariantCulture, out d);
+}
+
+public bool IsNumeric(string val, ref double d)
+{
+	if (val == null)
+	{
+		return false;
+	}
+	return Double.TryParse(val, System.Globalization.NumberStyles.AllowThousands | System.Globalization.NumberStyles.Float, System.Globalization.CultureInfo.InvariantCulture, out d);
+}
+
+
+]]></msxsl:script>
 </xsl:stylesheet>";
         
         private const string _strArgList = @"<ExtensionObjects />";
