7775d71dec117c479a6f2ee0dfa49c2ad1db8289
Shashank Bhosekar <sbhosekar@corcentric.com>
2024-08-22 13:32:55 -0400

    added for msi

--- Deployment/CreateMSI_UAT.ps1
+++ Deployment/CreateMSI_UAT.ps1
@@ -1,25 +1,28 @@
 ﻿
-Write-Host("Building MSI from scrip") -Fore Cyan
+Write-Host("Building MSI from script") -Fore Cyan
+
 # Build mode for building the BizTalk applications, can be Release or Debug
 $mode = "Debug"
 
 # Directory where the finished MSI installers will be placed
 $installersDir = "C:\Users\sbhosekar\Documents\Visual Studio 2012\Projects\DemoProjectForGitHubCICDPipeline\DemoDICDPipleineForVS\MSI1"
 
+# Options used by MSBuild
+$options = " /nologo /t:Installer /p:Configuration=" + $mode
  
 # Use MSBuild to build the installer
-$build = "C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe " + "`"C:\Users\sbhosekar\Documents\Visual Studio 2012\Projects\DemoProjectForGitHubCICDPipeline\DemoDICDPipleineForVS\Deployment\Deployment.btdfproj`"" + " /nologo /t:Installer /p:Configuration=" + "Debug"
- 
+$build = "C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" + " `"C:\actions-runner\DemoDICDPipleineForVS\DemoDICDPipleineForVS\Deployment\Deployment.btdfproj`"" + $options 
+
+
+# Copy the installer to the directory containing all installation files
+
+#Copy-Item ("C:\actions-runner\DemoDICDPipleineForVS\DemoDICDPipleineForVS\Deployment\bin\Debug\DemoProjectForGitHubCICDPipeline-1.0.0.msi") "C:\Users\sbhosekar\Documents\Visual Studio 2012\Projects\DemoProjectForGitHubCICDPipeline\DemoDICDPipleineForVS\MSI1" -Force
 
  
 # Build the installer
-
 Invoke-Expression $build
-
-Copy-Item ("C:\ShashankWork\TestMSI\TESTMSI\Deployment\bin\Debug" + "*.msi") $installersDir -Force
 
 $verbosity = " /v:q "
 
- 
 
  
