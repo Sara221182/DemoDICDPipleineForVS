5f628e41386e753309232821eacc9d11fa626d0f
Sara221182 <shashankmb@gmail.com>
2024-08-26 09:30:09 -0400

    Update CreateMSI_UAT.ps1

--- Deployment/CreateMSI_UAT.ps1
+++ Deployment/CreateMSI_UAT.ps1
@@ -1,6 +1,4 @@
 ﻿
- if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit } 
-
 # Use MSBuild to build the installer
 $build = "C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" + " C:\CICDTest\DemoDICDPipleineForVS\Deployment\Deployment.btdfproj" 
 
