4b9eb4b8305d1ecdf1fc0013cf93f1ff4b4aee94
Sara221182 <shashankmb@gmail.com>
2024-08-26 09:47:30 -0400

    Update CreateMSI_UAT.ps1

--- Deployment/CreateMSI_UAT.ps1
+++ Deployment/CreateMSI_UAT.ps1
@@ -1,4 +1,12 @@
-﻿
+
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{   
+  $arguments = "& '" + $myinvocation.mycommand.definition + "'"
+  Start-Process powershell -Verb runAs -ArgumentList $arguments
+  Break
+}
+
+Set-Location $PSScriptRoot
 # Use MSBuild to build the installer
 $build = "C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" + " C:\CICDTest\DemoDICDPipleineForVS\Deployment\Deployment.btdfproj" 
 
