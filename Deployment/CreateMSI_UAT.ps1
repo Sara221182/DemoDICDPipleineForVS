df975550d65d5e6f915b9dd28076fa750fd6338c
Sara221182 <shashankmb@gmail.com>
2024-08-27 13:31:06 -0400

    Update CreateMSI_UAT.ps1

--- Deployment/CreateMSI_UAT.ps1
+++ Deployment/CreateMSI_UAT.ps1
@@ -1,29 +1,4 @@
- 
- # Get the ID and security principal of the current user account
- $myWindowsID=[System.Security.Principal.WindowsIdentity]::GetCurrent()
- $myWindowsPrincipal=new-object System.Security.Principal.WindowsPrincipal($myWindowsID)
-
- # Get the security principal for the Administrator role
- $adminRole=[System.Security.Principal.WindowsBuiltInRole]::Administrator
-
- 
-    # We are not running "as Administrator" - so relaunch as administrator
-
-    # Create a new process object that starts PowerShell
-    $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
-
-    # Specify the current script path and name as a parameter
-    $newProcess.Arguments = $myInvocation.MyCommand.Definition;
-
-    # Indicate that the process should be elevated
-    $newProcess.Verb = "runas";
-
-    # Start the new process
-    [System.Diagnostics.Process]::Start($newProcess);
-
-    # Exit from the current, unelevated, process
-    exit
-    
+   
 
 
 # Use MSBuild to build the installer
